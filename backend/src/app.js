c197550e90c5b8015dda00c3a0ff71f90edfcf77
RitamDas30 <ritamdas238@gmail.com>
2025-10-09 15:47:53 +0530

    DB connected, Fix : .env

---
+++ backend/src/app.js
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
+import { connectDB } from "./config/db.js";
+
+// import authRoutes from "./routes/auth.routes.js";
+// import eventRoutes from "./routes/event.routes.js";
+// import registrationRoutes from "./routes/registration.routes.js";
+// import analyticsRoutes from "./routes/analytics.routes.js";
+  
+dotenv.config();
+connectDB();  
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+// Routes
+// app.use("/api/auth", authRoutes);
+// app.use("/api/events", eventRoutes);
+// app.use("/api/registrations", registrationRoutes);
+// app.use("/api/analytics", analyticsRoutes);
+
+// Default route
+app.get("/", (req, res) => {
+  res.send("APP is running...");
+});
+
+export default app;
