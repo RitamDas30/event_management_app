9721022a329cfdf8226a7d4c49b3425ddc0ca1eb
RitamDas30 <ritamdas238@gmail.com>
2025-10-12 23:57:11 +0530

    Final 2.1

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,11 +1,23 @@
-// frontend/src/utils/socket.js
+// import { io } from "socket.io-client";
+
+// const API = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+
+// const socket = io(API, { 
+//     transports: ["websocket", "polling"], 
+// });
+
+// export default socket;
+
+
+
+
+
+
 import { io } from "socket.io-client";
-
-const API = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
-// const BASE = API.replace(/\/api$/, ""); // remove /api if present
-
-const socket = io(API, { 
-    transports: ["websocket", "polling"], // Added polling for stability
+const SOCKET_ROOT_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+const socket = io(SOCKET_ROOT_URL, { 
+    transports: ["websocket", "polling"], 
+    withCredentials: true,
 });
 
 export default socket;
